ede34a5bde2750925e32013ca7a223a2156d182a
‘zhangsan’ <1455141477@qq.com>
2022-11-06 16:25:43 +0800

    third commit

--- cloud-providerconsul-payment8005/src/main/java/com/atguigu/springcloud/controller/TestGit.java
+++ cloud-providerconsul-payment8005/src/main/java/com/atguigu/springcloud/controller/TestGit.java
@@ -1,4 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("当前时master分支");
+        System.out.println("**********************");
+    }
 }
